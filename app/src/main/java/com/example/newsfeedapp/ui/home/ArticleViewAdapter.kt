8548d99913073fdf33760b2cfdaf04ecae7410f5
Nixon L <nicklazaro14@gmail.com>
2024-05-06 17:35:33 -0400

    welp.

--- app/src/main/java/com/example/newsfeedapp/ui/home/ArticleViewAdapter.kt
+++ app/src/main/java/com/example/newsfeedapp/ui/home/ArticleViewAdapter.kt
@@ -11,43 +11,94 @@ import com.bumptech.glide.Glide
 import com.example.newsfeedapp.R
 import com.example.newsfeedapp.model.Article
 import com.example.newsfeedapp.model.ArticlesListResponse
+import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class ArticleViewAdapter(
-    private val context: Context,
-) :
-    RecyclerView.Adapter<ArticleViewAdapter.ViewHolder>() {
-        private val articleList = mutableListOf<Article>()
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.fragment_home, parent, false))
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val article = articleList[position]
-        holder.bind(article)
-    }
-
-    override fun getItemCount() = articleList.size
-
-    fun setArticles(articles: List<Article>){
-        articleList.clear()
-        articleList.addAll(articles)
-    }
-
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val articlePubDate =  itemView.findViewById<TextView>(R.id.article_pub_date)
-        private val articleHeadline = itemView.findViewById<TextView>(R.id.article_headline)
-        private val articleSnippet = itemView.findViewById<TextView>(R.id.article_snippet)
-        fun bind(article: Article){
-            val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss+SSS", Locale.getDefault())
-            val date = dateFormat.parse(article.pubDate)
-            val newDateFormat = SimpleDateFormat("yyyy/MM/dd", Locale.getDefault())
-            articlePubDate.text = newDateFormat.format(date)
-            articleHeadline.text = article.headline?.main
-            articleSnippet.text = article.snippet
+/**
+ * Recycler view of articles from search result
+ */
+class ArticleViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private val articleList: MutableList<Article> = ArrayList()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return if (viewType == VIEW_TYPE_ARTICLE) {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.fragment_home, parent, false)
+            ArticleViewHolder(view)
+        } else  {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.fragment_article_first_page, parent, false)
+            FirstPageArticleViewHolder(view)
         }
+    }
 
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is FirstPageArticleViewHolder) {
+            holder.firstPageHeader.text = holder.itemView.context.getString(R.string.first_page, articleList[position].sectionName)
+        }
+        if (holder is ArticleViewHolder) {
+            val article = articleList[position]
+            val articleViewHolder = holder
+            articleViewHolder.headlineView.text = article.headline!!.main
+            articleViewHolder.snippetView.text = article.snippet
+            try {
+                val utcDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss+SSS", Locale.getDefault())
+                val date = utcDateFormat.parse(article.pubDate)
+                val newDateFormat = SimpleDateFormat("yyyy/MM/dd", Locale.getDefault())
+                articleViewHolder.dateView.text = newDateFormat.format(date)
+            } catch (e: ParseException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (position >= articleList.size) {
+            VIEW_TYPE_LOADING
+        } else if ("1" == articleList[position].printPage) {
+            VIEW_TYPE_FIRST_PAGE_ARTICLE
+        } else {
+            VIEW_TYPE_ARTICLE
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return articleList.size
+    }
+
+    fun setNewArticles(articles: List<Article>?) {
+        articleList.clear()
+        articleList.addAll(articles!!)
+    }
+
+    fun addArticles(articles: List<Article>?) {
+        articleList.addAll(articles!!)
+    }
+
+    internal class LoadingViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    internal open class ArticleViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val headlineView: TextView
+        val snippetView: TextView
+        val dateView: TextView
+
+        init {
+            dateView = view.findViewById(R.id.article_pub_date)
+            headlineView = view.findViewById(R.id.article_headline)
+            snippetView = view.findViewById(R.id.article_snippet)
+        }
+    }
+
+    internal class FirstPageArticleViewHolder(view: View) : ArticleViewHolder(view) {
+        val firstPageHeader: TextView
+
+        init {
+            firstPageHeader = view.findViewById(R.id.first_page_header)
+        }
+    }
+
+    companion object {
+        const val VIEW_TYPE_LOADING = 0
+        const val VIEW_TYPE_ARTICLE = 1
+        const val VIEW_TYPE_FIRST_PAGE_ARTICLE = 2
     }
 }
-
--- app/src/main/java/com/example/newsfeedapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/newsfeedapp/ui/home/HomeFragment.kt
@@ -1,5 +1,8 @@
 package com.example.newsfeedapp.ui.home
 
+import android.content.Context
+import androidx.recyclerview.widget.RecyclerView
+import androidx.core.widget.ContentLoadingProgressBar
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -7,92 +10,114 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.SearchView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.appcompat.widget.SearchView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.newsfeedapp.R
 import com.example.newsfeedapp.model.Article
 import com.example.newsfeedapp.network.APIClient
 import com.example.newsfeedapp.network.CallbackResponse
 
-class HomeFragment : Fragment() {
-
+class HomeFragment
+    : Fragment() {
     private val client = APIClient()
     private var recyclerView: RecyclerView? = null
+    private var progressSpinner: ContentLoadingProgressBar? = null
     private var savedQuery: String? = null
-    private var articleViewAdapter: ArticleViewAdapter? = null
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        // Inflates the layout for this fragment
-        val view = inflater.inflate(R.layout.article_result_list, container, false)
-        val recyclerView = view.findViewById<View>(R.id.rvArticleList) as RecyclerView
-        val context = view.context
-        recyclerView.layoutManager = GridLayoutManager(context, 2, GridLayoutManager.VERTICAL, false)
-        this.recyclerView = recyclerView
-
-        articleViewAdapter = ArticleViewAdapter(context)
-        recyclerView.adapter = articleViewAdapter
-
-        // Fetch default articles on initial load
-        fetchDefaultArticles()
-
-        return view
-    }
-
-    private fun fetchDefaultArticles() {
-        client.getArticlesByInput(object : CallbackResponse<List<Article>> {
-            override fun onSuccess(model: List<Article>) {
-                val adapter = recyclerView?.adapter as ArticleViewAdapter
-                adapter.setArticles(model)
-                adapter.notifyDataSetChanged()
-            }
-
-            override fun onFailure(error: Throwable?) {
-                Log.e("HomeFragment", error!!.message!!)
-            }
-        }, "New York")
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.search_menu, menu)
-        super.onCreateOptionsMenu(menu, inflater)
-    }
-
+    private var scrollListener: EndlessRecyclerViewScrollListener? = null
+    var adapter = ArticleViewAdapter()
     override fun onPrepareOptionsMenu(menu: Menu) {
         val item = menu.findItem(R.id.search).actionView as SearchView
-        item.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+        item.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
             override fun onQueryTextSubmit(query: String): Boolean {
-                updateAdapter(query)
+                loadNewArticlesByQuery(query)
                 return false
             }
 
-            override fun onQueryTextChange(newText: String?): Boolean {
+            override fun onQueryTextChange(newText: String): Boolean {
                 return true
             }
         })
     }
 
-    private fun updateAdapter(query: String) {
-        savedQuery = query
-        if (query.isEmpty()) {
-            fetchDefaultArticles()
-        } else {
-            client.getArticlesByInput(object : CallbackResponse<List<Article>> {
-                override fun onSuccess(model: List<Article>) {
-                    val adapter = recyclerView?.adapter as ArticleViewAdapter
-                    adapter.setArticles(model)
-                    adapter.notifyDataSetChanged()
-                }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+        retainInstance = true
+    }
 
-                override fun onFailure(error: Throwable?) {
-                    Log.e("HomeFragment", error!!.message!!)
-                }
-            }, query)
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater){
+        inflater.inflate(R.menu.search_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.article_result_list, container, false)
+        val localRecyclerView = view.findViewById<RecyclerView>(R.id.rvArticleList)
+        recyclerView = localRecyclerView
+        val context = view.context
+        val linearLayoutManager = LinearLayoutManager(context)
+        localRecyclerView.layoutManager = linearLayoutManager
+        localRecyclerView.adapter = adapter
+        scrollListener = object : EndlessRecyclerViewScrollListener(linearLayoutManager) {
+            override fun onLoadMore(page: Int, totalItemsCount: Int, view: RecyclerView?) {
+                loadNextDataFromApi(page)
+            }
         }
+        localRecyclerView.addOnScrollListener(scrollListener!!)
+
+        // Loads default article search
+        if (savedQuery.isNullOrBlank()){
+            loadNewArticlesByQuery("New York City")
+        }
+        return view
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+    }
+
+    private fun loadNewArticlesByQuery(query: String) {
+        Log.d("ArticleResultFragment", "load article with query $query")
+        progressSpinner?.show()
+        savedQuery = query
+        scrollListener?.resetState()
+        client.getArticlesByInput(object : CallbackResponse<List<Article>> {
+            override fun onSuccess(model: List<Article>) {
+                val adapter = recyclerView?.adapter as ArticleViewAdapter
+                adapter.setNewArticles(model)
+                adapter.notifyDataSetChanged()
+                Log.d("ArticleResultFragment", "successfully loaded articles")
+                progressSpinner?.hide()
+            }
+
+            override fun onFailure(error: Throwable?) {
+                Log.d("ArticleResultFragment", "failure load article " + error!!.message)
+                progressSpinner?.hide()
+            }
+        }, query)
+    }
+
+    private fun loadNextDataFromApi(page: Int) {
+        client.getArticlesByInput(object : CallbackResponse<List<Article>> {
+            override fun onSuccess(models: List<Article>) {
+                val adapter = recyclerView?.adapter as ArticleViewAdapter
+                adapter.addArticles(models)
+                adapter.notifyDataSetChanged()
+                Log.d("ArticleResultFragment", String.format("successfully loaded articles from page %d", page))
+            }
+
+            override fun onFailure(error: Throwable?) {
+                Log.d("ArticleResultFragment", "failure load article " + error!!.message)
+                Toast.makeText(context, error.message, Toast.LENGTH_SHORT).show()
+            }
+        }, savedQuery, page)
     }
 }
